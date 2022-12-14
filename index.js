94d17346fc3e1ac3567015360238cc0e13542cf8
RakindGarg01 <rakind0907.be20@chitkara.edu.in>
2022-12-14 15:55:59 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from 'dotenv'
+import questionRoutes from './routes/Questions.js'
+import answerRoutes from './routes/Answers.js'
+import userRoutes from './routes/user.js'
+import path from 'path';
+
+const app = express();
+dotenv.config();
+app.use(express.json({limit:"30mb",extended : true}))
+app.use(express.urlencoded({limit : "30mb" , extended : true}))
+app.use(cors());
+
+app.get('/' , (req,res) =>{
+    res.send("This is a Stack OverFlow Clone API")
+})
+
+app.use('/user' , userRoutes)
+app.use('/questions' , questionRoutes)
+app.use('/answer' , answerRoutes)
+//STATIC FILES
+app.use(express.static(path.join(__dirname,'./client/build')))
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname , "./client/build/index.html"))
+})
+
+const PORT = process.env.PORT || 8000;
+
+const DATABASE_URL = process.env.CONNECTION_URL
+mongoose.connect(DATABASE_URL ,{useNewUrlParser : true , useUnifiedTopology : true})
+    .then(() => {
+        console.log("Connection With DataBase is Succesfully Estabilished.");
+    })
+    .catch((err) => {
+        console.log("OOPS!! You lost Your Connection");
+    })
+
+app.listen(PORT , (err)=>{console.log(`Server is running on port ${PORT}` , err)})
+
+//C:\Users\rakin\Desktop\STACKOVERFLOW-DEVELOPMENT-APP
