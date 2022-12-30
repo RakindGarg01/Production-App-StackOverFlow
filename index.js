dd0a5a4333d2ae382bb25adc3237a48946ac3d92
RakindGarg01 <rakind0907.be20@chitkara.edu.in>
2022-12-30 23:40:26 +0530

    comunity

--- index.js
+++ index.js
@@ -2,21 +2,29 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from 'dotenv'
+import bodyParser from "body-parser";
 import questionRoutes from './routes/Questions.js'
 import answerRoutes from './routes/Answers.js'
 import userRoutes from './routes/user.js'
 import path from 'path';
 import { fileURLToPath } from 'url';
+import blogRoutes from './routes/Blogs.js'
 
 const app = express();
 dotenv.config();
 app.use(express.json({limit:"30mb",extended : true}))
 app.use(express.urlencoded({limit : "30mb" , extended : true}))
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const router = express.Router()
+
 
 app.use('/user' , userRoutes)
 app.use('/questions' , questionRoutes)
 app.use('/answer' , answerRoutes)
+app.use('/community', blogRoutes)
+
 //STATIC FILES
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
